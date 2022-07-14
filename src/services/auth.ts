1933e72e82efc3ddc9357cd7e160b341fcc03123
AnhHoangQuach <hoanganh36.work@gmail.com>
2022-07-14 11:24:20 +0700

    minor

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,7 +1,7 @@
 import { UserLoginType, GetUserData } from 'types/Auth';
 import { client } from './axios';
 
-const login = (body: UserLoginType) => client.post(`/auth/login`, body);
+const login = (body: UserLoginType) => client.post(`/admin/login`, body);
 const getProfile = (): Promise<GetUserData> => client.get(`/auth/me`);
 
 export default {
